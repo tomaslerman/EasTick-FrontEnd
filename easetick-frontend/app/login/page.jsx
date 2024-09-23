d21089bf3867fe87c676842d06f48ba9628e4d68
Nicolas232323 <nicolas.simone321@gmail.com>
2024-09-23 20:15:56 -0300

    CSS A LOGIN

--- easetick-frontend/app/login/page.jsx
+++ easetick-frontend/app/login/page.jsx
@@ -1,14 +1,17 @@
 import { login, signup } from './actions'
+import styles from './page.module.css'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <label htmlFor="email" className={styles.label}>Email:</label>
+        <input id="email" name="email" type="email" className={styles.input} required />
+        <label htmlFor="password" className={styles.label}>Password:</label>
+        <input id="password" name="password" type="password" className={styles.input} required />
+        <button type="submit" className={styles.button} formAction={login}>Log in</button>
+        <button type="button" className={`${styles.button} ${styles.buttonSecondary}`} formAction={signup}>Sign up</button>
+      </form>
+    </div>
   )
 }
